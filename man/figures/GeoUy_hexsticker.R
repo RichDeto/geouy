9ffec0b184be904bc1f7257e81f1d43cc66e83ef
Richard Detomasi <richard.detomasi@gmail.com>
2019-12-30 11:13:07 -0300

    svg simplificado

--- man/figures/GeoUy_hexsticker.R
+++ man/figures/GeoUy_hexsticker.R
@@ -35,7 +35,7 @@ as.numeric(object.size(s_s)) / as.numeric(  object.size(s))
 ### .png
 plot_a <-
         ggplot() +
-        geom_sf(data = s, fill = NA, size = .3, color = "#5796c905") +
+        # geom_sf(data = d_s, fill = NA, size = .3, color = "#5796c905") +
         geom_sf(data = s_s, fill = NA, size = .2, color = "#4dc0d1") +
         theme_void() +
         theme(panel.grid.major = element_line(colour = "transparent")) +
@@ -54,7 +54,7 @@ sticker(plot_a, package = "",
 ### .svg
 plot_y_svg <-
         ggplot() +
-        geom_sf(data = s, fill = NA, size = .08, color = "#5796c905") +
+        # geom_sf(data = d_s, fill = NA, size = .08, color = "#5796c905") +
         geom_sf(data = s_s, fill = NA, size = .35, color = "#4dc0d1") +
         theme_void() +
         theme(panel.grid.major = element_line(colour = "transparent")) +
@@ -67,6 +67,8 @@ plot_y_svg <-
 sticker(plot_y_svg, package = "",
         s_x = 1.03, s_y = 1, s_width = 1.5, s_height = 1.5, # ggplot image size and position
         h_fill = "black", h_color = "black", # hexagon
+        url = "https://github.com/RichDeto/geouy", u_color = "#4dc0d1",
+        spotlight = T, l_height = 17,
         filename = "./man/figures/geouy_logo_a.svg")  # output name and resolution
 
 
