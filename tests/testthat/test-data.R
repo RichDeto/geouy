13199031acb2531dc2026638851db29b1fd4e78d
richdeto <richard.detomasi@gmail.com>
2020-01-16 20:55:14 -0300

    tested

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,6 +1,6 @@
 test_that("connections working", {
-  # expect_is(load_geouy("Secciones"), "sf")
-  # expect_is(load_geouy("Rutas"), "sf")
-  # expect_is(load_geouy("Peajes"), "sf")
+  expect_is(load_geouy("Secciones"), "sf")
+  expect_is(load_geouy("Rutas"), "sf")
+  expect_is(load_geouy("Peajes"), "sf")
 })
 
