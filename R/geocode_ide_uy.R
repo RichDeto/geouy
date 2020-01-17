aaff5f9aa14e615459dd02574daced935bdcc59f
richdeto <richard.detomasi@gmail.com>
2020-01-17 01:54:59 -0300

    geocode

--- R/geocode_ide_uy.R
+++ R/geocode_ide_uy.R
@@ -3,12 +3,14 @@
 #' @keywords geocoding IDE_uy
 #' @return The DafaFrame x with the coordinates variables append (x and y)
 #' @importFrom dplyr mutate filter %>%
-#' @importFrom stringr str_sub str_locate str_trim
+#' @importFrom stringr str_sub str_locate str_trim str_replace_all
+#' @importFrom xml2 read_html
 #' @export
 #' @examples
 #'\dontrun{
-#' # x <- data.frame(cbind(dpto="Montevideo",loc="Montevideo",dir="Av. 18 de julio 1453"))
-#' # geocode_ide_uy(x)
+#' # x1 <- cbind(dpto="Montevideo",loc="Montevideo",dir="Av. 18 de julio 1453")
+#' # x2 <- data.frame(x1, stringsAsFactors = F)
+#' # geocode_ide_uy(x2)
 #'}
 
 geocode_ide_uy <- function(x) {
@@ -18,13 +20,13 @@ geocode_ide_uy <- function(x) {
   stopifnot(is.character(x$dir), "dir" %in% colnames(x))
   x <- x %>% mutate(dir = str_trim(dir)) %>% filter(nchar(dir) > 0)
   for (i in 1:nrow(x)) {
-    p <- suppressWarnings(readLines(paste0("http://servicios.ide.gub.uy/servicios/BusquedaDireccion?departamento=",
-                                           x[i,"dpto"] , "&localidad=", x[i,"loc"], "&calle=", x[i,"dir"])))
+    p <- suppressWarnings(read_html(str_replace_all(paste0("http://servicios.ide.gub.uy/servicios/BusquedaDireccion?departamento=",
+                                           x[i,"dpto"] , "&localidad=", x[i,"loc"], "&calle=", x[i,"dir"])," ","%20")))
     if (length(p) == 0) {
       next
     }
-    x[i,"x"] <- as.numeric(str_sub(p, str_locate(p, "puntoX\":")[2] + 1, str_locate(p, "puntoX\":")[2] + 10))
-    x[i,"y"] <- as.numeric(str_sub(p, str_locate(p, "puntoY\":")[2] + 1, str_locate(p, "puntoY\":")[2] + 10))
+    x[i,"x"] <- suppressWarnings(as.numeric(str_sub(p, str_locate(p, "puntoX\":")[2] + 1, str_locate(p, "puntoX\":")[2] + 10)))
+    x[i,"y"] <- suppressWarnings(as.numeric(str_sub(p, str_locate(p, "puntoY\":")[2] + 1, str_locate(p, "puntoY\":")[2] + 10)))
     p <- NULL
   }
   return(x)
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,5 +2,9 @@ test_that("connections working", {
   expect_is(load_geouy("Secciones"), "sf")
   expect_is(load_geouy("Rutas"), "sf")
   expect_is(load_geouy("Peajes"), "sf")
+  expect_false(is.na(sf::st_crs(load_geouy("Secciones", crs = 4326))))
+  # expect_equal(round(geocode_ide_uy(data.frame(cbind(dpto="Montevideo",
+  #                                                    loc="Montevideo",
+  #                                                    dir="Av. 18 de julio 1453"), 
+  #                                              stringsAsFactors = F))[4], 0), -56)
 })
-
