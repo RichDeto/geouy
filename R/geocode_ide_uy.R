2a918d88fefeb1291a8aa14abe4a55af5fa7c9b3
Richard Detomasi <richard.detomasi@gmail.com>
2020-01-17 15:16:09 -0300

    other touch for geocode

--- R/geocode_ide_uy.R
+++ R/geocode_ide_uy.R
@@ -20,11 +20,9 @@ geocode_ide_uy <- function(x) {
   stopifnot(is.character(x$dir), "dir" %in% colnames(x))
   x <- x %>% mutate(dir = str_trim(dir)) %>% filter(nchar(dir) > 0)
   for (i in 1:nrow(x)) {
-    p <- suppressWarnings(read_html(str_replace_all(paste0("http://servicios.ide.gub.uy/servicios/BusquedaDireccion?departamento=",
-                                           x[i,"dpto"] , "&localidad=", x[i,"loc"], "&calle=", x[i,"dir"])," ","%20")))
-    if (length(p) == 0) {
-      next
-    }
+    p <- suppressWarnings(xml2::read_html(str_replace_all(paste0("http://servicios.ide.gub.uy/servicios/BusquedaDireccion?departamento=",
+                                           x[i,"dpto"] , "&localidad=", x[i,"loc"], "&calle=", x[i,"dir"])," ","%20"),
+                          encoding = "UTF-8", options = c("NOBLANKS", "NOERROR"), n = 256))
     x[i,"x"] <- suppressWarnings(as.numeric(str_sub(p, str_locate(p, "puntoX\":")[2] + 1, str_locate(p, "puntoX\":")[2] + 10)))
     x[i,"y"] <- suppressWarnings(as.numeric(str_sub(p, str_locate(p, "puntoY\":")[2] + 1, str_locate(p, "puntoY\":")[2] + 10)))
     p <- NULL
