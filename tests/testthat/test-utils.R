c6f73731b2231d1dbd9202b09eac0ec77f00af36
Richard Detomasi <richard.detomasi@gmail.com>
2020-01-28 12:16:22 -0300

    test 4326

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,4 +4,6 @@ test_that("crs parameter working", {
   a <- load_geouy("Peajes")
   testthat::expect_error(is.uy4326(a))
   testthat::expect_equal(is.uy32721(a), "Your object have 100% of matches with Ururguay CRS 32721" )
+  b <- load_geouy("Peajes", 4326)
+  testthat::expect_equal(is.uy4326(b), "Your object have 100% of matches with Ururguay CRS 4326" )
 })
