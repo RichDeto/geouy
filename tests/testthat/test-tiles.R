573f08f1eef2e9365ef43d65eae78d58fdf0218e
richdeto <richard.detomasi@gmail.com>
2020-02-22 11:53:01 -0300

    again2

--- tests/testthat/test-tiles.R
+++ tests/testthat/test-tiles.R
@@ -5,10 +5,6 @@ test_that("connection to IDE server working", {
   skip_on_cran() # "IDEuy response time takes too long for testing times"
   skip_on_appveyor()
   
-  x <- data.frame(cbind(dpto = "Montevideo",
-                        loc = "Montevideo",
-                        dir = "Av. 18 de julio 1453"),
-                  stringsAsFactors = F)
   x1 <- sf::st_as_sf(data.frame(x = -55.184901, y = -34.905193), coords = c("x","y"), crs = 4326)
   x_tiles <- tiles_ide_uy(x1)
   expect_is(x_tiles, "stack")
