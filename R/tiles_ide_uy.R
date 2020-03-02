7b676ac1aaf5172fc5463428f86d7f8cd6a84380
richdeto <richard.detomasi@gmail.com>
2020-03-02 20:05:00 -0300

    tiles

--- R/tiles_ide_uy.R
+++ R/tiles_ide_uy.R
@@ -1,5 +1,6 @@
-#' This function allows to Download .tif files from the IDEuy orthophotos repository, according to a 'sf' object bbox.
+#' This function allows to Download .jpg files from the IDEuy tiles repository, according to a 'sf' object bbox.
 #' @param x An 'sf' object with the same crs as the homonym parameter
+#' @param format Format of the archives to download (avaiable: "jpg" and "tif") Default "jpg"
 #' @param folder Folder where are the files or be download
 #' @keywords IDE orthophotos Uruguay
 #' @return raster::stack object with th cropped tif corresponding to x bbox
@@ -19,15 +20,15 @@
 #' x_tiles <- tiles_ide_uy(x)
 #'}
 
-tiles_ide_uy <- function(x, folder = tempdir()){
+tiles_ide_uy <- function(x, format = "jpg", folder = tempdir()){
   # checks ----
   try(if (!is(x, "sf")) stop("The object you want to process is not class sf"))
   if (!is.character(folder) | length(folder) != 1) {
     message(glue::glue("You must enter a valid directory..."))
   }
   # warnings ----
-  if (length(fs::dir_ls(folder, regexp = "\\.tif$")) != 0) {
-    message(glue::glue("There are other .tif files in the folder that will be read..."))
+  if (length(fs::dir_ls(folder, regexp = "\\.jpg$")) != 0) {
+    message(glue::glue("There are other .jpg files in the folder that will be read..."))
   }
   # download ----
   try(dir.create(folder))
@@ -35,7 +36,19 @@ tiles_ide_uy <- function(x, folder = tempdir()){
   bb = x %>% sf::st_transform(32721) %>% sf::st_bbox() %>% as.vector() %>% raster::extent() %>% as('SpatialPolygons')
   raster::crs(bb) <- "+proj=utm +zone=21 +south +datum=WGS84 +units=m +no_defs"
   x2 <- geouy::load_geouy("Grilla ortofotos nacional", crs = crs) %>% sf::st_join(x, left = F) %>% filter(duplicated(.data$nombre))
-  a <- glue::glue("https://visualizador.ide.uy/descargas/CN_Remesa_{stringr::str_pad(x2$remesa, 2, pad = '0')}/02_Ortoimagenes/02_RGBI_8bits/{as.character(x2$nombre)}_RGBI_8_Remesa_{stringr::str_pad(x2$remesa, 2, pad = '0')}.tif")
+  
+  if (format == "jpg") {
+  a <- glue::glue("https://visualizador.ide.uy/descargas/CN_Remesa_{stringr::str_pad(x2$remesa, 2, pad = '0')}/02_Ortoimagenes/03_RGB_8bits/{as.character(x2$nombre)}_RGB_8_Remesa_{stringr::str_pad(x2$remesa, 2, pad = '0')}")
+  if (!file.exists(a)) {
+    message(glue::glue("Trying to download..."))
+    try(utils::download.file(a, glue::glue("{folder}//{basename(a)}/{c('.jpg', '.jgw')}")  , mode = "wb", method = "libcurl"))
+  } else {
+    message(glue::glue("Tiles already exists, the download is omitted"))
+  }
+  # read brick
+  ar <- fs::dir_ls(folder,  regexp = "\\.jpg$")
+  } if (format == "tif") {
+     a <- glue::glue("https://visualizador.ide.uy/descargas/CN_Remesa_{stringr::str_pad(x2$remesa, 2, pad = '0')}/02_Ortoimagenes/02_RGBI_8bits/{as.character(x2$nombre)}_RGBI_8_Remesa_{stringr::str_pad(x2$remesa, 2, pad = '0')}.tif")
   if (!file.exists(a)) {
     message(glue::glue("Trying to download..."))
     try(utils::download.file(a, glue::glue("{folder}//{basename(a)}"), mode = "wb", method = "libcurl"))
@@ -44,7 +57,11 @@ tiles_ide_uy <- function(x, folder = tempdir()){
   }
   # read brick
   ar <- fs::dir_ls(folder,  regexp = "\\.tif$")
-  a2 <- raster::brick(ar) 
+  } else {
+    stop("The format you want ")
+  }
+ 
+  a2 <- raster::raster(ar) 
   a3 <- raster::crop(a2, bb)
   raster::plotRGB(a3)
   return(a3)
