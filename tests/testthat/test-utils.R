323ef2b0de01cf16f1ed185f4b4ccca71f41659a
Richard Detomasi <richard.detomasi@gmail.com>
2020-03-09 15:21:14 -0300

    utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,11 @@
-context("Testing which_uy")
+context("Testing utils")
 library(dplyr);library(sf)
-test_that("crs parameter working", {
+testthat::test_that("crs parameter working", {
   
-  skip_on_cran()
+  testthat::skip_on_cran()
   
   a <- load_geouy("Peajes")
-  testthat::expect_error(is.uy4326(a))
+  # testthat::expect_error(is.uy4326(a))
   testthat::expect_equal(is.uy32721(a), "Your object have 100% of matches with Ururguay CRS 32721")
   b <- load_geouy("Peajes", 4326)
   testthat::expect_equal(is.uy4326(b), "Your object have 100% of matches with Ururguay CRS 4326")
