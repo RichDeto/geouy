b7913f710c901eebda29ccfdbe653c404b99d388
Richard Detomasi <richard.detomasi@gmail.com>
2020-03-12 13:17:22 -0300

    v0.1.8

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -5,12 +5,15 @@ test_that("connections working", {
   skip_on_cran()
   
   testthat::expect_is(load_geouy("Secciones"), "sf")
-  p <- load_geouy("Playas")
-  testthat::expect_is(p, "sf")
+  testthat::expect_warning(load_geouy("Playas"))
   testthat::expect_is(load_geouy("Centros poblados pg"), "sf")
 })
 
 test_that("crs parameter working", {
+  
+  testthat::expect_error(load_geouy("Playas", folder = 1))
+  testthat::expect_error(load_geouy("Playas", folder = c("c://", "E://")))
+  
   a <- load_geouy("Secciones", crs = 4326)
   a1 <- sf::st_crs(a)
   testthat::expect_equal(a1[1], structure(list(epsg = 4326L), .Names = "epsg"))
