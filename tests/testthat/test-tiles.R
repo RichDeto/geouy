d9d10599abff02c96866909c2390bd665f94bf21
Richard Detomasi <RichDeto@users.noreply.github.com>
2020-03-16 23:59:27 -0300

    Update test-tiles.R

--- tests/testthat/test-tiles.R
+++ tests/testthat/test-tiles.R
@@ -1,11 +1,15 @@
 context("Testing tiles")
 
-test_that("Directory error", {
-  
-  x <- sf::st_as_sf(data.frame(x = c(-56.14449, -56.13936), y = c(-34.805237, -34.806787)), coords = c("x","y"), crs = 4326)
+test_that("test errors", {
+  x0 <- data.frame(x = c(-56.14449, -56.13936), y = c(-34.805237, -34.806787))
+  testthat::expect_error(tiles_ide_uy(x0) 
+  x <- sf::st_as_sf(x0, coords = c("x","y"), crs = 4326)
   testthat::expect_error(tiles_ide_uy(x, folder = 1))
   testthat::expect_error(tiles_ide_uy(x, folder = c("c://", "E://")))
   testthat::expect_error(tiles_ide_uy(x, format = "gtif"))
+  x <- sf::st_as_sf(data.frame(x = c(--58.89550, -58.892143), y = c(-34.562668, -34.564435)), coords = c("x","y"), crs = 4326)
+  testthat::expect_error(tiles_ide_uy(x, urban = TRUE))
+  testthat::expect_error(tiles_ide_uy(x, urban = FALSE))
 })
 
 test_that("connection to IDE server for tif", {
