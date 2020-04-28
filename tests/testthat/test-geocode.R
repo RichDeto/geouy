7cc8bb240908862a01a392dbaba039760b69f719
richdeto <richard.detomasi@gmail.com>
2020-04-28 16:38:54 -0300

    test geocode on appveyor and travis

--- tests/testthat/test-geocode.R
+++ tests/testthat/test-geocode.R
@@ -3,8 +3,8 @@ context("Testing geocode")
 test_that("connection to IDE server working", {
   
   skip_on_cran() # "IDEuy response time takes too long for testing times"
-  skip_on_appveyor()
-  skip_on_travis()
+  # skip_on_appveyor()
+  # skip_on_travis()
  
   x <- data.frame(cbind(dpto = "Montevideo", loc = "Montevideo", dir = "Av. 18 de julio 1453"), stringsAsFactors = F)
   x1 <- geocode_ide_uy(x)
