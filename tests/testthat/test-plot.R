4f83e3165084607fc527ed1ce6b128831ae3cc5c
richdeto <richard.detomasi@gmail.com>
2020-04-29 16:39:23 -0300

    other tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,12 +2,12 @@ context("Testing plot_geouy")
 
 test_that("output is a ggplot", {
   secc <- load_geouy("Secciones")
-  aaa <- plot_geouy(x = secc, a = "AREA")  
+  aaa <- plot_geouy(x = secc, col = "AREA")  
   expect_is(aaa, "ggplot")
 })
 
 test_that("Plot uses correct data", {
   secc <- load_geouy("Secciones")
-  p <- plot_geouy(secc)
-  expect_that(secc, equals(p$data))
+  p <- plot_geouy(secc, col = "AREA")
+  testthat::expect_equal(secc, p$data)
 })
