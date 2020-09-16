133c0bb97a2abda020df0a50f641ae5f295aac85
richdeto <richard.detomasi@gmail.com>
2020-09-16 18:56:30 -0300

    v0.2.3

--- tests/testthat/test-where_uy.R
+++ tests/testthat/test-where_uy.R
@@ -4,4 +4,5 @@ test_that("multiplication works", {
   expect_warning(expect_error(where_uy(c = "Localidades pg", d = "cod", e = "c")))
   expect_error(where_uy(c = "Localidades pg", d = "name", e = 1))
   expect_equal(nrow(where_uy(c = "Peajes", d = "name", e = "Pando")), 1)
+  expect_error(where_uy(c = "Localidades pg", d = "cod", e = 1220))
 })
