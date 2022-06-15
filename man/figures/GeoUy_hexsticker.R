57462ed161cc706954592aaa1ae0103102cbfa85
RichDeto <richard.detomasi@gmail.com>
2022-06-15 11:22:49 -0300

    stickers

--- man/figures/GeoUy_hexsticker.R
+++ man/figures/GeoUy_hexsticker.R
@@ -17,6 +17,7 @@ d <- load_geouy("Departamentos", crs = 4326)
 s <- load_geouy("Secciones", crs = 4326)
 
 # Simplify geometry
+sf_use_s2(F)
 d_s <- st_simplify(d, preserveTopology = TRUE, dTolerance = .1)
 s_s <- st_simplify(s, preserveTopology = TRUE, dTolerance = .01)
 
@@ -99,19 +100,19 @@ sticker(plot_b, package = "",
 plot_b_svg <-
   ggplot() +
   # geom_sf(data = d_s, fill = NA, size = .08, color = "#5796c905") +
-  geom_sf(data = s_s, fill = NA, size = .35, color = "#4dc0d1") +
+  geom_sf(data = s_s, fill = NA, size = .35, color = "black") +
   theme_void() +
   theme(panel.grid.major = element_line(colour = "transparent")) +
   #  theme(legend.position = "none") +
-  annotate("text", x = -54.7, y = -30.8, label = "geouy", color = "#4dc0d1",
+  annotate("text", x = -54.7, y = -30.8, label = "geouy", color = "black",
            size = 6, family = "Roboto", fontface = "bold", angle = -35) # (.png  size = 25)(.svg  size = 6)
 
 
 
 sticker(plot_b_svg, package = "",
         s_x = 1.03, s_y = 1, s_width = 1.5, s_height = 1.5, # ggplot image size and position
-        h_fill = "black", h_color = "black", # hexagon
-        url = "https://github.com/RichDeto/geouy", u_color = "#4dc0d1",
+        h_fill = "#4dc0d1", h_color = "#4dc0d1", # hexagon
+        url = "https://github.com/RichDeto/geouy", u_color = "black",
         spotlight = T, l_height = 17,
         filename = "./man/figures/geouy_logo_b.svg")  # output name and resolution
 
