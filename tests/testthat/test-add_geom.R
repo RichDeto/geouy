f9eb7918c38426ae4bdfc847f494812e68e48163
Richard Detomasi <RichDeto@users.noreply.github.com>
2022-07-04 21:43:28 -0300

    Update test-add_geom.R

--- tests/testthat/test-add_geom.R
+++ tests/testthat/test-add_geom.R
@@ -2,5 +2,5 @@ test_that("add_geom works", {
   pobre_x_dpto <- as.data.frame(cbind(nomdpto = c("ARTIGAS", "DURAZNO", "FLORIDA", "LAVALLEJA"),
                         Pobreza = c(0.26, 0.27, 0.07, 0.10)))
   pobre_x_dpto_geo <- add_geom(data = pobre_x_dpto, unit = "Departamentos", variable = "nomdpto")
-  testthat::expect_equal(ncol(pobre_x_dpto_geo), 4)
+  testthat::expect_equal(ncol(pobre_x_dpto_geo), 3)
 })
