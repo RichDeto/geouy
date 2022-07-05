6374dcc6391260c460ce6a67f0712b673982cd43
richdeto <richard.detomasi@gmail.com>
2022-07-04 22:28:56 -0300

    v0.2.6

--- tests/testthat/test-add_geom.R
+++ tests/testthat/test-add_geom.R
@@ -1,6 +1,6 @@
 test_that("add_geom works", {
   pobre_x_dpto <- as.data.frame(cbind(nomdpto = c("ARTIGAS", "DURAZNO", "FLORIDA", "LAVALLEJA"),
                         Pobreza = c(0.26, 0.27, 0.07, 0.10)))
-  pobre_x_dpto_geo <- add_geom(data = pobre_x_dpto, unit = "Departamentos", variable = "nomdpto")
+  pobre_x_dpto_geo <- add_geom(data = pobre_x_dpto, unit = "Deptos", variable = "nomdpto")
   testthat::expect_equal(ncol(pobre_x_dpto_geo), 3)
 })
